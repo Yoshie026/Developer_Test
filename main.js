fa2a198d7bce869473c8be6070cddcf86834cef9
yoshi-cat <yoshitsugu.kosaka@more.global>
2023-12-12 19:06:54 +0000

    progress made

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-// import './style.css'
-import { setupCounter } from './counter.js'
+var btn = document.getElementsByClassName("btn");
+var i;
+for (i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
-
-
-
-
-
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-    
-//   </div>
-// `
-
-setupCounter(document.querySelector('#counter'))
+function collapseMenu(){
+ btn[0].classList.toggle("active");
+    var content = btn[0].nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+}
+collapseMenu();
